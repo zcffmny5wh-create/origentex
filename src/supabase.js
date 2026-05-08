d4b4e4a5add886df22a132fee309dfa460762b36
MARLON BAENA <crjmoda@iMac.local>
2026-05-07 23:14:21 -0500

    fix supabase hardcode

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  "https://vsdlmymzrerhmitbjyki.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzZGxteW16cmVyaG1pdGJqeWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzYyMjUsImV4cCI6MjA5Mzc1MjIyNX0.ZSrxLbeb5-p8tXIv4K9CNwQlAk3V3YAPqPxe_MoZoOc"
 );
